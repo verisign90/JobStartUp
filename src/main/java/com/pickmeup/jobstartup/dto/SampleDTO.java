a72d20a314fabab99120aa0155fcebce65499a5f
SUHUN <favud20@gmail.com>
2023-09-27 17:51:52 +0900

    Create base and sample

---
+++ src/main/java/com/pickmeup/jobstartup/dto/SampleDTO.java
@@ -0,0 +1,14 @@
+package com.pickmeup.jobstartup.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SampleDTO {
+
+    private int id;
+    private String password;
+}
