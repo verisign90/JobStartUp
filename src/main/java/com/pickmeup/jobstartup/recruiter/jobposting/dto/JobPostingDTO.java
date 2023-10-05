560f9eb5535bc6428acc8c1d53fbd1b2ffa9853d
kang <ydj98088@gmail.com>
2023-10-05 19:00:51 +0900

    채용공고 등록글 작성중

---
+++ src/main/java/com/pickmeup/jobstartup/recruiter/jobposting/dto/JobPostingDTO.java
@@ -0,0 +1,36 @@
+package com.pickmeup.jobstartup.recruiter.jobposting.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class JobPostingDTO {
+
+    private Number company_no;
+    private Number posting_no;
+    private String posting_title;
+    private String posting_career;
+    private String posting_academy;
+    private String posting_jobCode;
+    private String posting_type;
+    private String posting_labor;
+    private String posting_preferential;
+    private String posting_skill;
+    private Number posting_salary;
+    private Timestamp posting_sdate;
+    private Timestamp posting_edate;
+    private Number posting_cnt;
+    private String isshow;
+    private String posting_content;
+    private String posting_working_day;
+    private String posting_position;
+
+
+}
