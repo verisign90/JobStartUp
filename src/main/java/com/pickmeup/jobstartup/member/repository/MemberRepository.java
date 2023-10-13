a78ed52ea9f42b48d996ee1a66e6e1aab05b2c70
verisign_90 <verisign_90@naver.com>
2023-10-13 10:34:45 +0900

    일반회원 시큐리티 적용

--- src/main/java/com/pickmeup/jobstartup/member/repository/MemberRepository.java
+++ src/main/java/com/pickmeup/jobstartup/member/repository/MemberRepository.java
@@ -9,10 +9,16 @@ import org.springframework.stereotype.Repository;
 public interface MemberRepository {
     //아이디 중복 확인
     int countByMemberId(String memberId);
+
     //회원가입
     int save(Member member);
+
     //회원 구분에 따른 메뉴ID 가져오기
     String selectMenuId(String memberType);
+
     //이메일 중복 확인
     int countByEmail(String email);
+
+    //스프링 시큐리티를 위한 것. 아이디로 멤버 가져오기
+    Member findByUsername(String username);
 }
